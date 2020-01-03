a528941ce4f2cf0d5e2a701ab58c04bb7be14f35
Tarena <Tarena@DESKTOP-DQR1NLH>
2020-01-03 11:50:29 +0800

    3

--- github1/src/github1/test/test1.java
+++ github1/src/github1/test/test1.java
@@ -3,7 +3,7 @@ package github1.test;
 public class test1 {
 public static void main(String[] args) {
 	System.out.println("hello world");
-	System.out.println();
+	System.out.println("a");
 
 	int a = 1;
 }
